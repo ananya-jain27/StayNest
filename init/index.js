e0ee1263bdaefb7c7e4dacbcac7592a78e377b96
Ananya Jain <mkjainananya27@gmail.com>
2025-09-18 18:18:16 +0530

    some changes

--- init/index.js
+++ init/index.js
@@ -1,10 +1,16 @@
+if(process.env.NODE_ENV != "production"){
+    require('dotenv').config()
+}
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
 //connection build with mongoDB 
+let dbUrl = process.env.ATLASDB_URL;
+
 async function main(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/staynest');
+    // await mongoose.connect('mongodb://127.0.0.1:27017/staynest');
+    await mongoose.connect(dbUrl);
 }
 main().then(() => {
     console.log("connection build");
